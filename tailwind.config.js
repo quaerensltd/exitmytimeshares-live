812076ee0637384d49a5ff59fe5b20b49b62d8a5
Jeroen Brussel <quaerensltd@consultant.com>
2025-06-27 14:34:07 +0100

    Rebuild CSS into docs/ and include docs in purge paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./public/**/*.html",          // all HTML in public/
-      "./src/**/*.{html,js,jsx,ts}"  // your source templates/scripts
+      "./docs/**/*.html",     // <-- add this
+      "./src/**/*.{html,js,jsx,ts,tsx}"
     ],
     theme: { extend: {} },
     plugins: [],
